10f2293c47fd752b0857f8950538d3180cba62dd
Jos3C190 <josecarloslopezm17@gmail.com>
2025-02-26 18:11:10 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoC1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoC1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
