1252053bfd6975ade751cd1315f134e73002d1db
Jos3C190 <josecarloslopezm17@gmail.com>
2025-02-28 19:17:20 -0600

    Agregando CRUD

--- ProyectoC1/Controllers/AlumnosController.cs
+++ ProyectoC1/Controllers/AlumnosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProyectoC1.Db;
+using ProyectoC1.Models;
 
 namespace ProyectoC1.Controllers
 {
@@ -16,6 +17,52 @@ namespace ProyectoC1.Controllers
         {
             var alumnos = _dbConn.Alumnos.ToList();
             return View(alumnos);
+        }
+
+        [HttpGet]
+        public IActionResult UpSert(int id)
+        {
+            if (id == 0)
+            {
+                Alumno alumno = new();
+                return View(alumno);
+            }
+            else
+            {
+                Alumno alumno = _dbConn.Alumnos.FirstOrDefault(row => row.AlumnoId == id) ?? new();
+                return View(alumno);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Alumno model)
+        {
+            if (model.AlumnoId == 0)
+            {
+                if (ModelState.IsValid)
+                {
+                    _dbConn.Alumnos.Add(model);
+                    _dbConn.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                _dbConn.Alumnos.Update(model);
+                _dbConn.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Alumno alumno = _dbConn.Alumnos.FirstOrDefault(row => row.AlumnoId == id) ?? new();
+            alumno.IsActive = false;
+            _dbConn.Alumnos.Update(alumno);
+            _dbConn.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- ProyectoC1/Models/Alumno.cs
+++ ProyectoC1/Models/Alumno.cs
@@ -13,20 +13,28 @@ namespace ProyectoC1.Models
         public int AlumnoId { get; set; }
 
         [NotNull]
+        [Required(ErrorMessage = "Nombre es requerido")]
         public string Nombres { get; set; }
 
         [NotNull]
+        [Required(ErrorMessage = "Apellido es requerido")]
         public string Apellidos { get; set; }
 
         [NotNull]
+        [Required(ErrorMessage = "Edad es requerido")]
         public int Edad { get; set; }
 
         [NotNull]
+        [Required(ErrorMessage = "Carnet es requerido")]
         public string Carnet { get; set; }
+
+        [Required(ErrorMessage = "Cantidad de materias es requerido")]
         public int CantidadMaterias { get; set; }
+
+        [Required(ErrorMessage = "Ciclo es requerido")]
         public int Ciclo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Fecha de nacimiento es requerido")]
         public DateTime FechaNacimiento { get; set; }
 
         [NotMapped]
@@ -34,6 +42,14 @@ namespace ProyectoC1.Models
         #endregion Propiedades
 
         #region Constructor
+
+        public Alumno()
+        {
+            FechaNacimiento = DateTime.Now.Date;
+            IsActive = true;
+            Created = DateTime.Now;
+            CreatedBy = "ADMIN";
+        }
         public Alumno(string nombres, int edad, string apellidos, DateTime fechaNacimiento)
         {
             Nombres = nombres;
